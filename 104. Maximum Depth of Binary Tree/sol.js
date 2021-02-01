b0506c8f0c0debc803b4665db1a61caca9965614
Ashish1323 <ashishagnihotri2k@gmail.com>
2021-02-01 21:42:24 +0530

    104. Maximum Depth of Binary Tree

---
+++ 104. Maximum Depth of Binary Tree/sol.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(!root) return 0
+    let x=maxDepth(root.left)
+    let y=maxDepth(root.right)
+    return x>y?x+1:y+1
+  };

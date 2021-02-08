82676b49e22553d18beaa0d3386f692a2bd96306
Ashish1323 <ashishagnihotri2k@gmail.com>
2021-02-08 13:11:05 +0530

    111. Minimum Depth of Binary Tree

---
+++ 111. Minimum Depth of Binary Tree/sol.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if(!root) return null
+    if(!root.left && !root.right) return 1
+    if(!root.left) return 1 + minDepth(root.right)
+    if(!root.right) return 1 + minDepth(root.left)
+    return 1 + Math.min(minDepth(root.left), minDepth(root.right))
+};
+

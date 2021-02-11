210794f28f55f69d79b610f6d0afdfe25b8b300b
Ashish1323 <ashishagnihotri2k@gmail.com>
2021-02-11 17:23:40 +0530

    222. Count Complete Tree Nodes

---
+++ 222. Count Complete Tree Nodes/sol.py
@@ -0,0 +1,11 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        if not root: return 0
+        return self.countNodes(root.left) + self.countNodes(root.right) + 1
+        

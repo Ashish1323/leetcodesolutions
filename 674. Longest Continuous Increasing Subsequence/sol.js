5d4df87e89c756d7032d7130eb174830510aeb2c
Ashish1323 <ashishagnihotri2k@gmail.com>
2021-03-25 12:47:25 +0530

    674. Longest Continuous Increasing Subsequenc better spacee

--- 674. Longest Continuous Increasing Subsequence/sol.js
+++ 674. Longest Continuous Increasing Subsequence/sol.js
@@ -16,3 +16,25 @@ var findLengthOfLCIS = function(nums) {
   }
     return  Math.max(...arr)
 };
+
+
+// Better space.
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var findLengthOfLCIS = function(nums) {
+//     if(nums.length==0) return 0
+//   var max=0
+//   var newMax=0
+//   for(i=0;i<nums.length;i++){
+//           if(nums[i]<nums[i+1]) max+=1
+//           else{
+//               max+=1
+//             newMax=Math.max(newMax,max)
+//               max=0
+//           }
+//   }
+//     return  newMax
+// };

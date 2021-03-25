4d35bca9dc1c8dadf884d36d02073522278e854b
Ashish1323 <ashishagnihotri2k@gmail.com>
2021-03-25 12:44:37 +0530

    674. Longest Continuous Increasing Subsequence

---
+++ 674. Longest Continuous Increasing Subsequence/sol.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLengthOfLCIS = function(nums) {
+    if(nums.length==0) return 0
+  var max=0
+  var arr=[]
+  for(i=0;i<nums.length;i++){
+          if(nums[i]<nums[i+1]) max+=1
+          else{
+              max+=1
+            arr.push(max)
+              max=0
+          }
+  }
+    return  Math.max(...arr)
+};
